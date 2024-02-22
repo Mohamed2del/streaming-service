ad59232f31729c029ee822c453d15a4abfc79b0c
Mohamed Adel <Mohamedadelmohamed997@gmail.com>
2024-02-23 01:57:15 +0200

    cors

--- [BE] streaming-service/src/main/java/javaservices/streamingservice/Controller/VideoStreamingController.java
+++ [BE] streaming-service/src/main/java/javaservices/streamingservice/Controller/VideoStreamingController.java
@@ -7,6 +7,7 @@ import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ import java.nio.channels.FileChannel;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-
+@CrossOrigin(origins = "http://localhost:4200") // Replace with your Angular app's domain
 @RestController
 public class VideoStreamingController {
 
